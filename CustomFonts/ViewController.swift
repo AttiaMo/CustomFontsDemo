ef69da23f11307637a01f00b1acca2f416ca2125
Atia M <atiamohamedein@gmail.com>
2017-11-01 14:00:34 +0200

    Initial Commit

---
+++ CustomFonts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomFonts
+//
+//  Created by Atia M2 on 11/1/17.
+//  Copyright © 2017 Attia Mo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
